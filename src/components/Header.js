3076727317efe1f98f80f04a59ca501b23cb9ba5
satyam <tech.kumarsatyam@gmail.com>
2023-10-26 11:14:26 +0530

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
               variant="h6"
               className={classes.title}
             >
-              Crypto State
+              Crypto Stats
             </Typography>
             <Select
               variant="outlined"
