734d67d9e79553880921fca65975ae8feebc8692
satyam <tech.kumarsatyam@gmail.com>
2023-11-02 15:57:03 +0530

    update banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,14 +3,23 @@ import Carousel from "./Carousel";
 
 const useStyles = makeStyles((theme) => ({
   banner: {
-    // backgroundImage: "url(./bannermain.jpg)",
-    backgroundImage:"url(./bannermain.jpg)",
+    // backgroundImage:"url(./bannermain.jpg)",
+    backgroundImage:"url(https://images.pexels.com/photos/814136/pexels-photo-814136.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load)",
+    // backgroundImage:"url(https://images.pexels.com/photos/3745234/pexels-photo-3745234.jpeg)",
+    // backgroundImage:"url(https://images.pexels.com/photos/12154830/pexels-photo-12154830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)",
+    backgroundSize: "cover", // Set the background size to cover the entire container
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center", // Center the background image
+    height: "90vh",
+    
+
   },
+
   bannerContent: {
-    height: 400,
+    height: 550,
     display: "flex",
     flexDirection: "column",
-    paddingTop: 15,
+    // paddingTop: 2,
     justifyContent: "space-around",
   },
   tagline: {
@@ -47,8 +56,9 @@ function Banner() {
           <Typography
             variant="subtitle2"
             style={{
-              color: "darkgrey",
-              // color: "white",
+              // color: "darkgrey",
+              color: "white",
+              fontWeight: "bold",
               textTransform: "capitalize",
               fontFamily: "Montserrat",
             }}
